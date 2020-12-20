055c8d18b9f2334b40d5b404e6bdd7213468917d
Ankit Karn <ankit.karn.ak@gmail.com>
2020-12-20 23:06:56 +0530

    added html, css, js file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Na'vi Translator
+
+This is fun translator website which convert from English to Navi. Na'vi is a constructed language spoken by the Na'vi people in the fictional world Pandora in James Cameron's blockbuster movie Avatar.
---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/navi.json";
+
+function errorHandler(error) {
+    console.log("error occured ", error);
+    alert("some error occured, please try again after sometimes")
+}
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(resonse => resonse.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
